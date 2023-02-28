0a176c8448eb3ac858deaaeb13fcb6783d1e5dee
Stanka Dalekova <stanka.dalekova@bgosoftware.com>
2023-02-28 14:40:01 +0200

    Name Changes

---
+++ EFMirationPlayground/Model/ExpenseHistory.cs
@@ -0,0 +1,11 @@
+﻿namespace EMigrationPlayground.view
+{
+    public class ExpenseHistory
+    {
+        public int Id { get; set; }
+        public int ExpenseItemId { get; set; }
+        public ExpenseItem? ExpenseItem { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+    }
+}
---
+++ EFMirationPlayground/Model/ExpenseItem.cs
@@ -0,0 +1,10 @@
+﻿namespace EMigrationPlayground.view
+{
+    public class ExpenseItem
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Category { get; set; }
+        public List<ExpenseHistory> History { get; set; } = new List<ExpenseHistory>();
+    }
+}
